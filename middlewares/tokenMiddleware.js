b48e9acac851a1ef732d855c73f5985b1bffbe1f
Dominik Lenart <dominik.lenart84@gmail.com>
2023-06-23 11:14:35 +0200

    update hw04

--- middlewares/tokenMiddleware.js
+++ middlewares/tokenMiddleware.js
@@ -1,16 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 const tokenMiddleware = async (req, res, next) => {
-	const token = req.headers.authorization?.split(' ')[1];
-	if (!token) {
-		return res.sendStatus(401).json({ message: 'Not authorized' });
-	}
-	jwt.verify(token, process.env.ACCESS_TOKEN, (err, data) => {
-		if (err) {
-			return res.sendStatus(403);
+	try {
+		const token = req.headers.authorization?.split(' ')[1];
+		if (!token) {
+			return res.status(401).json({ message: 'Not authorized' });
 		}
+		const data = jwt.verify(token, process.env.ACCESS_TOKEN);
 		req.user = data;
 		next();
-	});
+	} catch (err) {
+		if (err.name === 'JsonWebTokenError') {
+			return res.status(403).json({ message: 'Invalid token' });
+		}
+		return next(err);
+	}
 };
+
 module.exports = tokenMiddleware;
