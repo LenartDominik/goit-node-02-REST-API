c97ff7f8b59c976e0e02263bbd9a92aae8cc7467
Dominik Lenart <dominik.lenart84@gmail.com>
2023-07-04 15:27:24 +0200

    update hw06

---
+++ controllers/emailOperations.js
@@ -0,0 +1,39 @@
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
+const { SENDGRID_API_KEY, PORT, BASE_URL, SENDER } = process.env;
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const baseUrl = BASE_URL + ':' + PORT;
+
+const sendVerificationMsg = async (addressee, verificationToken, errors) => {
+  const msg = {
+    to: addressee,
+    from: {
+      name: 'CONTACT APP',
+      email: SENDER,
+    },
+    subject: 'Confirm registration',
+    html: `
+      <p style="font-family: Arial, sans-serif;">
+        Hi there!
+      </p>
+      <p>Thank you for registering! We are thrilled to have you on board!</p>
+      <p>Just one quick step left to get started - please click the link below to confirm your email address.</p>
+      <p>${baseUrl}/api/users/verify/${verificationToken}</p>
+      <p>Error: ${errors[0].message}</p>
+    `,
+  };
+
+  await sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+module.exports = {
+	sendVerificationMsg,
+};
